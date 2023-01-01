f484a17565a35c4faca1dba6f3b9700b76ff4848
Jelson J <jelson20@outlook.com>
2023-01-01 23:41:01 +0000

    update css and html file

--- js/app.js
+++ js/app.js
@@ -1,44 +1,23 @@
-filterSelection('all');
-function filterSelection(c) {
-	let x, i;
-	x = document.getElementsByClassName('column');
-	if (c == 'all') c = '';
-	for (i = 0; i < x.length; i++) {
-		removeClass(x[i], 'show');
-		if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show');
-	}
-}
+//GALLERY FILTER
+const filterBnt = document.querySelector('#filter-btn').children;
+const allItems = document.querySelector('.work-gallery').children;
 
-function addClass(element, name) {
-	let i, arr1, arr2;
-	arr1 = element.className.split(' ');
-	arr2 = name.split(' ');
-	for (i = 0; i < arr2.length; i++) {
-		if (arr1.indexOf(arr2[i]) == -1) {
-			element.className += ' ' + arr2[i];
+for (let i = 0; i < filterBnt.length; i++) {
+	filterBnt[i].addEventListener('click', function () {
+		for (let a = 0; a < filterBnt.length; a++) {
+			filterBnt[a].classList.remove('active');
 		}
-	}
-}
+		this.classList.add('active');
+		const target = this.getAttribute('data-target');
 
-function removeClass(element, name) {
-	let i, arr1, arr2;
-	arr1 = element.className.split(' ');
-	arr2 = name.split(' ');
-	for (i = 0; i < arr2.length; i++) {
-		while (arr1.indexOf(arr2[i]) > -1) {
-			arr1.splice(arr1.indexOf(arr2[i]), 1);
+		for (let y = 0; y < allItems.length; y++) {
+			allItems[y].style.display = 'none';
+			if (target == allItems[y].getAttribute('data-id')) {
+				allItems[y].style.display = 'block';
+			}
+			if (target == 'all') {
+				allItems[y].style.display = 'block';
+			}
 		}
-	}
-	element.className = arr1.join(' ');
-}
-
-// Add active class to the current button (highlight it)
-let btnContainer = document.getElementById('mainBtn');
-let btns = btnContainer.getElementsByClassName('btn');
-for (let i = 0; i < btns.length; i++) {
-	btns[i].addEventListener('click', function () {
-		let current = document.getElementsByClassName('active');
-		current[0].className = current[0].className.replace(' active', '');
-		this.className += ' active';
 	});
 }
