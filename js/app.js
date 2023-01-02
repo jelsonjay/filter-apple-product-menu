331d39a9659d63963f13762210748bc58ccefb79
Jelson J <jelson20@outlook.com>
2023-01-02 20:09:16 +0000

    added new images

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
+'use strict';
 //GALLERY FILTER
 const filterBnt = document.querySelector('#filter-btn').children;
 const allItems = document.querySelector('.container').children;
+const topUp = document.querySelector('.top-up');
 
 for (let i = 0; i < filterBnt.length; i++) {
 	filterBnt[i].addEventListener('click', function () {
-		for (let a = 0; a < filterBnt.length; a++) {
-			filterBnt[a].classList.remove('active');
+		for (let j = 0; j < filterBnt.length; j++) {
+			filterBnt[j].classList.remove('active');
 		}
 		this.classList.add('active');
 		const target = this.getAttribute('data-target');
 
-		for (let y = 0; y < allItems.length; y++) {
-			allItems[y].style.display = 'none';
-			if (target == allItems[y].getAttribute('data-id')) {
-				allItems[y].style.display = 'block';
+		for (let i = 0; i < allItems.length; i++) {
+			allItems[i].style.display = 'none';
+			if (target == allItems[i].getAttribute('data-id')) {
+				allItems[i].style.display = 'block';
 			}
 			if (target == 'all') {
-				allItems[y].style.display = 'block';
+				allItems[i].style.display = 'block';
 			}
 		}
 	});
 }
+
+//SCROLL UP
+window.addEventListener('scroll', () => {
+	if (window.pageYOffset > 150) {
+		topUp.classList.add('active');
+	} else {
+		topUp.classList.remove('active');
+	}
+});
